3dc000c1cb9ce24be480ce06712c1e9fc3f2cdaf
احمد سعد احمد الكيلاني <aelkilany@bu.edu.sa>
2021-03-21 15:38:45 +0300

    21-03today work

--- تنفيذ رصد الإنقطاع.sql
+++ تنفيذ رصد الإنقطاع.sql
@@ -1,31 +1,34 @@
-
+/* Formatted on 21/03/2021 15:37:57 (QP5 v5.227.12220.39754) */
 DECLARE
    CURSOR get_reg_std
    IS
-      SELECT SYBSTDC_PIDM pidm, SYBSTDC_CRN crn,ssbsect_subj_code,ssbsect_crse_numb
-        FROM sybstdc,ssbsect
+      SELECT SYBSTDC_PIDM pidm,
+             SYBSTDC_CRN crn,
+             ssbsect_subj_code,
+             ssbsect_crse_numb
+        FROM sybstdc, ssbsect
        WHERE     SYBSTDC_TERM_CODE = :p_term
              AND SYBSTDC_DISCONNECTED = 'Y'
              AND f_get_styp (SYBSTDC_PIDM) <> 'д'
-             and ssbsect_term_code=:p_term 
-       and ssbsect_crn=SYBSTDC_CRN;
+             AND ssbsect_term_code = :p_term
+             AND ssbsect_crn = SYBSTDC_CRN;
 
    l_count_sfrstcr     NUMBER := 0;
    l_sgbstdn_sfrstcr   NUMBER := 0;
 
    CURSOR get_dc_students
    IS
-      SELECT distinct sfrstcr_pidm pidm
+      SELECT DISTINCT sfrstcr_pidm pidm
         FROM sfrstcr s
        WHERE     SFRSTCR_TERM_CODE = :p_term
              AND SFRSTCR_GRDE_CODE = 'гд'
-             AND SFRSTCR_RSTS_CODE in ( 'RE','RW')
+             AND SFRSTCR_RSTS_CODE IN ('RE', 'RW')
              AND NOT EXISTS
                         (SELECT '1'
                            FROM sfrstcr
                           WHERE     SFRSTCR_TERM_CODE = :p_term
                                 AND NVL (SFRSTCR_GRDE_CODE, '0') <> 'гд'
-                                AND SFRSTCR_RSTS_CODE in ( 'RE','RW')
+                                AND SFRSTCR_RSTS_CODE IN ('RE', 'RW')
                                 AND SFRSTCR_PIDM = s.SFRSTCR_PIDM);
 
 BEGIN
@@ -34,7 +37,8 @@ BEGIN
       UPDATE sfrstcr
          SET SFRSTCR_GRDE_CODE = 'гд',
              SFRSTCR_GRDE_CODE_MID = '0',
-             SFRSTCR_DATA_ORIGIN = 'Banner DC',sfrstcr_activity_date = SYSDATE
+             SFRSTCR_DATA_ORIGIN = 'Banner DC',
+             sfrstcr_activity_date = SYSDATE
        WHERE     SFRSTCR_TERM_CODE = :p_term
              AND SFRSTCR_PIDM = rec.pidm
              AND SFRSTCR_RSTS_CODE IN ('RE', 'RW')
@@ -50,15 +54,16 @@ BEGIN
    END LOOP;
 
    DBMS_OUTPUT.put_line ('SFRSTCR updated rows =' || l_count_sfrstcr);
-   
+
    --change student status >>
-   for rec2 in get_dc_students
-   loop
-   p_update_std_status (rec2.pidm,
+   FOR rec2 IN get_dc_students
+   LOOP
+      p_update_std_status (rec2.pidm,
                            :p_term,
                            'гд',
                            'Banner DC');
-   l_sgbstdn_sfrstcr := l_sgbstdn_sfrstcr + 1;
-   end loop ;
-      DBMS_OUTPUT.put_line ('SGBSTDN updated rows =' || l_sgbstdn_sfrstcr);
+      l_sgbstdn_sfrstcr := l_sgbstdn_sfrstcr + 1;
+   END LOOP;
+
+   DBMS_OUTPUT.put_line ('SGBSTDN updated rows =' || l_sgbstdn_sfrstcr);
 END;
