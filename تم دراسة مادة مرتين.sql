d3a1d93133e4338577e31600db53fc99f0816ecd
احمد سعد احمد الكيلاني <aelkilany@bu.edu.sa>
2021-03-22 09:30:53 +0300

    saving data

--- تم دراسة مادة مرتين.sql
+++ تم دراسة مادة مرتين.sql
@@ -1,9 +1,9 @@
-/* Formatted on 15/03/2021 15:30:26 (QP5 v5.227.12220.39754) */
-SELECT   F_GET_DESC_FNC ('STVCOLL', SGBSTDN_COLL_CODE_1, 30) college,
+/* Formatted on 22/03/2021 09:27:42 (QP5 v5.227.12220.39754) */
+SELECT F_GET_DESC_FNC ('STVCOLL', SGBSTDN_COLL_CODE_1, 30) college,
        F_GET_DESC_FNC ('STVDEPT', SGBSTDN_DEPT_CODE, 30) department,
        F_GET_STD_ID (G1.SHRTCKG_PIDM) STD_NAME,
        F_GET_STD_NAME (G1.SHRTCKG_PIDM) STD_ID,
-        F_GET_DESC_FNC ('STVSTYP', SGBSTDN_STYP_CODE, 30) STYPE,
+       F_GET_DESC_FNC ('STVSTYP', SGBSTDN_STYP_CODE, 30) STYPE,
        L.SHRTCKN_SUBJ_CODE || L.SHRTCKN_CRSE_NUMB SUBJ_CODE1,
        CAPP_MANIPULATION_FUNCTIONAL.F_GET_CRSE_TITLE (L.SHRTCKN_SUBJ_CODE,
                                                       L.SHRTCKN_CRSE_NUMB)
@@ -28,7 +28,7 @@ SELECT   F_GET_DESC_FNC ('STVCOLL', SGBSTDN_COLL_CODE_1, 30) college,
        AND L2.SHRTCKN_TERM_CODE = G2.SHRTCKG_TERM_CODE
        AND G2.SHRTCKG_TCKN_SEQ_NO = L2.SHRTCKN_SEQ_NO
        AND sgbstdn_stst_code NOT IN ('ŒÃ', '„Œ', 'ÿ”')
-      -- AND SGBSTDN_DEPT_CODE = '1502'
+       -- AND SGBSTDN_DEPT_CODE = '1502'
        AND G2.SHRTCKG_PIDM = G1.SHRTCKG_PIDM
        AND L2.SHRTCKN_PIDM = G2.SHRTCKG_PIDM
        AND L.SHRTCKN_TERM_CODE < L2.SHRTCKN_TERM_CODE
@@ -62,3 +62,16 @@ SELECT   F_GET_DESC_FNC ('STVCOLL', SGBSTDN_COLL_CODE_1, 30) college,
                                      AND R1.SHRGRDE_LEVL_CODE =
                                             R2.SHRGRDE_LEVL_CODE)
                       AND SHRGRDE_PASSED_IND = 'Y')
+                      --«÷«›… «·Œ›ﬁ«‰ ›Ï «·„—… «·À«‰Ì… 
+       AND EXISTS
+              (SELECT 1
+                 FROM SHRGRDE R1
+                WHERE     SHRGRDE_CODE = G2.SHRTCKG_GRDE_CODE_FINAL
+                      AND SHRGRDE_LEVL_CODE = SGBSTDN_LEVL_CODE
+                      AND SHRGRDE_TERM_CODE_EFFECTIVE =
+                             (SELECT MAX (R2.SHRGRDE_TERM_CODE_EFFECTIVE)
+                                FROM SHRGRDE R2
+                               WHERE     R1.SHRGRDE_CODE = R2.SHRGRDE_CODE
+                                     AND R1.SHRGRDE_LEVL_CODE =
+                                            R2.SHRGRDE_LEVL_CODE)
+                      AND SHRGRDE_PASSED_IND != 'Y')
